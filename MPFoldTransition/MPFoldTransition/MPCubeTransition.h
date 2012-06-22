9dd7f2b4d4f9f7f7da6e88dd788dda83249a0c0b
Christopher Bowns <github@cbowns.com>
2012-06-22 11:23:36 -0700

    Add MPCubeTransition from 100grams fork, commit 3fa5e41bc4cdb76b38bc

---
+++ MPFoldTransition/MPFoldTransition/MPCubeTransition.h
@@ -0,0 +1,13 @@
+//
+//  MPCubeTransition.h
+//  MPFoldTransition
+//
+//  Created by Rotem Rubnov on 15/5/2012.
+//  Copyright (c) 2012 Odyssey Computing. All rights reserved.
+//
+
+#import "MPFoldTransition.h"
+
+@interface MPCubeTransition : MPFoldTransition
+
+@end
